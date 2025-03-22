ff78917a27f2c50bff995aa314a373dbbb705fbe
moonblade <moonblade168@gmail.com>
2025-03-22 17:19:02 +0530

    Proper fxi

--- beetsapi.py
+++ beetsapi.py
@@ -14,7 +14,7 @@ from utils import custom_logger
 
 logger = custom_logger(__name__)
 
-if str(config["plugins"]).split(" "):
+if config["plugins"]:
     plugins.load_plugins(str(config["plugins"]).split(" "))
     lib = Library(os.path.join(BEETS_DIR, "library.db"), directory=config["directory"].get())
 
