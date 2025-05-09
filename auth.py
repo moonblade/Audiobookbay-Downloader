138ebcaefd991b723d9374a3ef51a01ad6c4a8da
moonblade <moonblade168@gmail.com>
2025-05-09 23:25:23 +0530

    User last seen time

--- auth.py
+++ auth.py
@@ -3,6 +3,7 @@ import os
 import base64
 import requests
 import uuid
+import time
 
 from constants import ADMIN_ID, ADMIN_PASS, ADMIN_USER, DB_PATH
 
@@ -30,6 +31,17 @@ def validate_key(key):
             return {"username": user.get("username", "invalid"), "role": user.get("role", "user"), "id": user.get("id", "0")}
     return None
 
+def update_last_seen(user_id):
+    users = get_users()
+    for user in users:
+        if user.get("id", "0") == user_id:
+            user["last_seen"] = int(time.time())
+            break
+    users_url = os.path.join(DB_PATH, "users.json")
+    with open(users_url, "w") as f:
+        f.write(json.dumps(users))
+    return True
+
 def validate_user(username, password):
     base64_password = base64.b64encode(password.encode("utf-8")).decode("utf-8")
     if username == ADMIN_USER and base64_password == ADMIN_PASS:
@@ -41,6 +53,7 @@ def validate_user(username, password):
             valid = base64.b64decode(base64_password).decode("utf-8") == password
             if not valid:
                 return None
+            update_last_seen(user.get("id", "0"))
             return {"username": username, "role": user.get("role", "user"), "id": user.get("id", "0")}
 
 def add_user(username, password):
@@ -54,7 +67,7 @@ def add_user(username, password):
 
 def get_users_list():
     users = get_users()
-    return [{"username": user.get("username", "invalid"), "role": user.get("role", "user"), "id": user.get("id", "0")} for user in users]
+    return [{"username": user.get("username", "invalid"), "role": user.get("role", "user"), "id": user.get("id", "0"), "last_seen": "never" if user.get("last_seen", 0) == 0 else time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(user.get("last_seen", 0)))} for user in users]
 
 def change_password(id, password):
     base64_password = base64.b64encode(password.encode("utf-8")).decode("utf-8")
